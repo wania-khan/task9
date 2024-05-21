8dee4312fe283f168a16bb4f57b7ec44a9dcc1f5
Wania Khan <waniak305@gmail.com>
2024-05-21 19:35:33 +0500

    upd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import HeroSection1 from './components/heroSection-1.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <HeroSection1 />
+    <span className="bg-gradient-to-r from-blue via-green to-indigo text-transparent bg-clip-text">powerful creators</span>
+    </>
   );
 }
 
