bbcb030fa22ed8570d0e694a962ad0f7e955c783
Wania Khan <waniak305@gmail.com>
2024-05-26 01:43:39 +0500

    upd

--- src/components/heroSection-1.js
+++ src/components/heroSection-1.js
@@ -11,16 +11,20 @@ function heroSection1(){
     </h1><br />
     <p className="text-center font-normal text-xl">Choose a plan that&apos;s right for you</p>
     <br /><br />
-    <div class="relative m-auto max-w-[520px] flex items-center justify-center gap-8 font-normal text-base">
+    <div class="relative m-auto max-w-[520px] flex items-center justify-center gap-8 font-normal text-base sm:max-w[320px]">
     <div className="">Pay Monthly</div>
     <div className="justify-center items-center">
     <label class="inline-flex items-center cursor-pointer">
      <input type="checkbox" value="" class="sr-only peer" />
-     <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+     <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-400 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
     </label>
      </div>  
     <div className="">Pay Yearly</div>
-    <div className="flex absolute items-center top-9 -right-11 font-medium text-lg sm:w-auto sm:h-auto">
+    <div className="lg:flex md:flex hidden absolute items-center top-9 -right-11 font-medium text-lg sm:w-auto sm:h-auto">
+      <img src={Arrow} className="w-24 h-18 -mt-7" alt="error-arrow" />
+      <p className="text-[#1D4ED8]">Save 25%</p>
+     </div>
+     <div className="flex items-center font-medium text-lg sm:w-auto sm:h-auto ">
       <img src={Arrow} className="w-24 h-18 -mt-7" alt="error-arrow" />
       <p className="text-[#1D4ED8]">Save 25%</p>
      </div>
